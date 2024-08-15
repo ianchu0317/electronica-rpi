b1081601f793fec3047270fcac9ec9b48b2c120e
Ian Chen <chenian317@gmail.com>
2024-08-15 17:39:16 -0300

    subir circuito pull up

---
+++ circuitos_simples/circuito_6_pull_up/control_pull_up_event.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+# configurar programa
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# configurar pin
+PIN = 18
+GPIO.setup(PIN, GPIO.IN, pull_up_down_=GPIO.PUD_UP)
+
+def press():
+	print("Interruptor presionado !")
+
+# waiting event
+GPIO.add_event_detect(PIN, GPIO.FALLING, callback=press)
+
+print("Esperando 10s")
+time.sleep(10)
+
+print("Limpiando GPIO")
+GPIO.cleanup()
