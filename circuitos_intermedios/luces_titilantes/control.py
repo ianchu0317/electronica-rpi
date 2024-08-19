f0df62a3b98d800ed9f0b75183004f7f92a29a3b
Ian Chen <chenian317@gmail.com>
2024-08-19 00:24:45 -0300

    update add trident led function

--- circuitos_intermedios/luces_titilantes/control.py
+++ circuitos_intermedios/luces_titilantes/control.py
@@ -1,6 +1,8 @@
+# -*- coding: cp1252 -*-
 import RPi.GPIO as GPIO
 import time
 from random import choice
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -9,13 +11,16 @@ GPIO.setmode(GPIO.BCM)
 led_pin = [14, 15, 18, 23, 24]  # RPI BCM PIN
 GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.LOW)
 
+
 # turn all LED ON
 def all_led_on():
 	GPIO.output(led_pin, GPIO.HIGH)
 
+
 # turn all LED OFF
 def all_led_off():
 	GPIO.output(led_pin, GPIO.LOW)
+
 
 # turn led one at the time
 def zig_zag_led():
@@ -30,6 +35,7 @@ def zig_zag_led():
 		time.sleep(0.06)
 		GPIO.output(led, GPIO.LOW)
 
+
 # turn led off one at the time
 def zig_zag_led_reverse():
 	all_led_on()
@@ -43,6 +49,7 @@ def zig_zag_led_reverse():
 		time.sleep(0.06)
 		GPIO.output(led, GPIO.HIGH)
 
+
 # random light up
 def random_led():
 	all_led_off()
@@ -51,6 +58,22 @@ def random_led():
 		GPIO.output(led, GPIO.HIGH)
 		time.sleep(0.03)
 		GPIO.output(led, GPIO.LOW)
+
+
+# shine led alternatively
+def trident_led():
+    all_led_off()
+    for led_index in range(len(led_pin)):
+        if led_index%2 == 0:
+            GPIO.output(led_pin[led_index], GPIO.HIGH)
+    time.sleep(0.5)
+    for led_index in range(len(led_pin)):
+        if led_index%2 != 0:
+            GPIO.output(led_pin[led_index], GPIO.HIGH)
+        else:
+            GPIO.output(led_pin[led_index], GPIO.LOW)
+    time.sleep(0.5)
+
 
 # main loop
 def main():
@@ -72,8 +95,12 @@ def main():
 		time.sleep(0.5)
 		all_led_off()
 		time.sleep(0.5)
+	# 3 times trident
+	for i in range(3):
+                trident_led()
 	# random led light up
 	random_led()
+
 
 if __name__ == "__main__":
 	main()
